a4282227ba878e8b8f4019a35a131c310cbffb8f
Teddy Martin <tkmartin@smith.edu>
2023-05-02 18:22:52 -0400

    created initial Room class

---
+++ Room.java
@@ -0,0 +1,14 @@
+public class Room {
+
+    String name;
+    int width;
+    int length;
+    int floor;
+
+    /* Default constructor */
+    public Room(String name, int width, int length, int floor) {
+        this.name = name;
+        this.width = width;
+        this.length = length;
+        this.floor = floor;
+    }
